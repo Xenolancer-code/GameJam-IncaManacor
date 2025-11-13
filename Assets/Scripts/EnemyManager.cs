3dff9c4162bd894533f0dd8d16def6ee33dbdfab
Xenolancer-code <sergimarques4@gmail.com>
2025-11-13 14:00:15 +0100

    Avanzar el manager de enemigos

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -1,12 +1,15 @@
 using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class EnemyMovment : MonoBehaviour
 {
+    [SerializeField] private float rotaX = 180;
     private GameObject player;
     private NavMeshAgent enemyAgent;
+    private GameObject rightArm;
 
     private void Awake()
     {
@@ -14,12 +17,15 @@ public class EnemyMovment : MonoBehaviour
     }
     void Start()
     {
+        rightArm = GameObject.Find("RightArm");
         player = GameObject.FindWithTag("Player");
     }
 
 
     void Update()
     {
+        float rotationValue = rotaX * Time.deltaTime;
+        transform.Rotate(rotationValue, 0, 0);
         if (player.transform.tag.Equals("Player"))
         {
             enemyAgent.SetDestination(player.transform.position);
