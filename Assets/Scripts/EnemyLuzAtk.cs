d4140a5220eeab9b1b4a6ed53918eca10c437777
Xenolancer-code <sergimarques4@gmail.com>
2026-02-19 13:19:46 +0100

    Soporte micro

--- Assets/Scripts/EnemyLuzAtk.cs
+++ Assets/Scripts/EnemyLuzAtk.cs
@@ -1,14 +1,29 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyLuzAtk : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private NavMeshAgent enemyAgent;
+    private Animator animator;
+    [Header("Attack")]
+    [SerializeField] private float attackCooldown = 1f;  // Tiempo entre ataques
+    [SerializeField] private float stopByAtackPlayer = 1.1f; // Tiempo que el enemigos se queda parado al atacar
+    [SerializeField] private float attackDistance;
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private LayerMask playerLayer;
+    private int hitPlayerHP = 1;
+    private bool playerInsideAttackRange = false;
+    private bool isAttacking = false;
+    private float lastAttackTime = 0f;
+    private bool playerIsDead = false;
+    
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
