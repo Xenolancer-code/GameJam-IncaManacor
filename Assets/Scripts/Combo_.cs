ba80b8e0fcf031193218fda684b6b8f1884a5be0
Xenolancer-code <sergimarques4@gmail.com>
2026-03-17 01:28:18 +0100

    a

--- Assets/Scripts/Combo_.cs
+++ Assets/Scripts/Combo_.cs
@@ -4,15 +4,13 @@ using UnityEngine.InputSystem;
 public class Combo_ : StateMachineBehaviour
 {
     [SerializeField] private int num;
- 
-
+    
     private int valor = 0;
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-   
+     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    // override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    // {
+    //      
+    // }
      //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
